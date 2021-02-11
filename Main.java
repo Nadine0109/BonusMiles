b8219964eb8c8d126187d82166ae81e5a89588f5
Nadine0109 <nadine0109@ya.ru>
2021-02-11 21:55:35 +0500

    Main.java added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int onebonusmile = 2000;
+        int amountinKopecks = 3_000_000;
+
+        int bonusmiles = amountinKopecks / onebonusmile;
+        System.out.println(bonusmiles);
+    }
+}
